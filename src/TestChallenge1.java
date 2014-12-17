1366393fd319da0d127d38bd2c2e08ab847c0ab5
mbrendsel <monica_brendsel@asl.org>
2014-12-17 09:01:05 +0000

    Update TestChallenge1.java

--- src/TestChallenge1.java
+++ src/TestChallenge1.java
@@ -1,3 +1,7 @@
+import org.asl.karelx.Squarebot;
+
+import edu.fcps.karel2.Display;
+
 /**
  * Read and implement all of the TODO items in the main method, below.
  * 
@@ -10,6 +14,17 @@ public class TestChallenge1 {
 		// TODO Create a Squarebot and have it place beepers at the corners of a rectangle
 		//   	with bottom right corner at (9, 3) and sized for 5 beepers on each side (see picture)
 		// TODO Create a Sentry and have it patrol clockwise around the beepers, indefinitely
+		
+		Display.openDefaultWorld();
+        Display.setSize(10, 10);
+        Display.setSpeed(10);
+        
+        Squarebot walter = new Squarebot(9, 3);
+        Sentry sam = new Sentry();
+       
+        walter.layCorners(5);
+        sam.patrol();
+		
 	}
 
 }
