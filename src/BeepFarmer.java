ce8402b9bf33083c1abe96978a4bea77828a0ef3
mbrendsel <monica_brendsel@asl.org>
2014-12-17 09:10:57 +0000

    Update BeepFarmer.java

--- src/BeepFarmer.java
+++ src/BeepFarmer.java
@@ -1,7 +1,5 @@
 import org.asl.karelx.Farmer;
-	
-	import edu.fcps.karel2.Display;
-	
+import edu.fcps.karel2.Display;
 	/**
 	 * Represents a farmer capable of sowing or reaping beepers from a rectangular garden
 	 *
