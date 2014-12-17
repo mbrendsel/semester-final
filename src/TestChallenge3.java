9b3811afc8eac975a816be6a8a44c2994147c07b
mbrendsel <monica_brendsel@asl.org>
2014-12-17 09:02:29 +0000

    Update TestChallenge3.java

--- src/TestChallenge3.java
+++ src/TestChallenge3.java
@@ -1,3 +1,5 @@
+import edu.fcps.karel2.Display;
+
 /**
  * Read and implement all of the TODO items in the main method, below.
  * 
@@ -10,6 +12,19 @@ public class TestChallenge3 {
 		// TODO Create an instance of a BeepFarmer with no beepers, anywhere on the farm
 		// TODO Have the farmer gather all the beepers from the garden plot in the center of the map
 		// TODO Have the farmer drop at location (1, 1) all the beepers picked and then move aside.
-	}
-
+		Display.openWorld("src/maps/farm.map");
+        	Display.setSize(15, 15);
+        	Display.setSpeed(5);
+        
+        BeepFarmer ben = new BeepFarmer();
+        
+        ben.reap(3, 6);
+        
+        ben.teleport(1, 1);
+        
+        while(ben.hasBeepers()){
+        	ben.putBeeper();
+        }
+        ben.sideStepRight();
+}
 }
